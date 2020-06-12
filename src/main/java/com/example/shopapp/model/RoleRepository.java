41af8ccdb8aa63bdd0a746cf96f77508d33a23b9
Adrian Baran <baranadrian98@gmail.com>
2020-06-13 00:32:54 +0200

    connected branches

--- src/main/java/com/example/shopapp/model/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopapp.model;
-
-import com.example.shopapp.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-    public Role findByRole(String role);
-}
