06cf6b16e34c11cb5efad6855445e1d8448d7443
Adrian Baran <baranadrian98@gmail.com>
2020-06-13 00:34:05 +0200

    connected branches

---
+++ src/main/java/com/example/shopapp/dto/ProductItemDto.java
@@ -0,0 +1,28 @@
+package com.example.shopapp.dto;
+
+import com.example.shopapp.model.Item;
+import com.example.shopapp.model.Product;
+import com.sun.istack.NotNull;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+public class ProductItemDto {
+
+    @NotBlank(message = "Name must be not blank")
+    private String name;
+    @NotNull
+    private float price;
+
+    private String description;
+
+    private LocalDateTime dateSold;
+
+    public ProductItemDto(Product product, Item item){
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.description = product.getDescription();
+        this.dateSold = item.getDateSold();
+    }
+
+}
